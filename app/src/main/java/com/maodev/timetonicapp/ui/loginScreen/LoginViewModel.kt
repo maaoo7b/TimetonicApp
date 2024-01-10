c135e4dd0ef16baf9581152ad0f94360463cc143
maodev <bmauricio.cruz07@gmail.com>
2024-01-10 16:03:30 -0500

    Loginpage screen finished

---
+++ app/src/main/java/com/maodev/timetonicapp/ui/loginScreen/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.maodev.timetonicapp.ui.loginScreen
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    private val _email = MutableLiveData<String>()
+    private val _password = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+    val password: LiveData<String> = _password
+
+    fun onLoginChanged(email:String, password:String){
+        _email.value = email
+        _password.value = password
+    }
+
+}
